3391952faacd90b0150c0c1ed0ed11bffbef672e
Pavel Gordienko <guzmo6@rambler.ru>
2019-04-08 09:20:11 +0300

    add stack

--- app.rb
+++ app.rb
@@ -3,7 +3,9 @@ doing = ['run', 'go', 'sit']
 
 Shoes.app :title => "Дружбинский" do
 		background white..gray
-	   button "Жми" do
-	     append { para "\n\n#{object.sample} #{doing.sample}" }
-	   end
+	   stack margin:10 do
+	   		   button "Жми" do
+	    		 	append { para "\n\n#{object.sample} #{doing.sample}" }
+	   			end
+		end
 	end
